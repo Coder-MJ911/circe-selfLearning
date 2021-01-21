8c399fbbb40bbbde6ca7bb5dc63022ec02e32b2f
Jiajie <jiajie.ma@thoughtworks.com>
2021-01-21 16:22:35 +0800

    make http4s model

---
+++ src/main/scala/http4s/HttpClientInstances.scala
@@ -0,0 +1,17 @@
+package http4s
+
+import cats.effect.{ContextShift, IO}
+import org.http4s.client.blaze.BlazeClientBuilder
+
+import scala.concurrent.ExecutionContext
+
+
+object HttpClientInstances {
+
+  def createClient(apiUrl: String) = {
+    val ec = ExecutionContext.global
+    implicit val sc: ContextShift[IO] = IO.contextShift(ec)
+    new SyncHttpClient(apiUrl, BlazeClientBuilder[IO](ec).resource)
+  }
+
+}
