3c2c2e36d188db70ce76b242344198a73a040e5f
Jiajie <jiajie.ma@thoughtworks.com>
2021-01-24 19:02:06 +0800

    add SemiAuto circe

--- src/main/scala/simpleCirce/semiAutomaticDerivation/SemiAuto.scala
+++ src/main/scala/simpleCirce/semiAutomaticDerivation/SemiAuto.scala
@@ -8,16 +8,24 @@ object SemiAuto {
   implicit val fooDecoder: Decoder[Foo] = deriveDecoder[Foo] //simply: deriveDecoder
   implicit val fooEncoder: Encoder[Foo] = deriveEncoder[Foo]
 
+  implicit val decodeUser: Decoder[User] = Decoder.forProduct3("id", "first_name", "last_name")(User.apply)
+  implicit val encodeUser: Encoder[User] = Encoder.forProduct3("id", "first_name", "last_name")(u => (u.id, u.firstName, u.lastName))
+
   def main(args: Array[String]): Unit = {
-    // Todo: have an Encoder or Decoder defined in code and semi-automatic derivation can help:
+    // Todo: first
     val fooJson = Foo(1, "first", c = true).asJson(fooEncoder) // simply: asJson
     val foo = fooJson.as[Foo](fooDecoder) // simply: as[Foo]
     println("foo = " + foo.toOption)
 
-    // Todo: circe-generic has a @JsonCodec annotation that simplifies the use of semi-automatic generic derivation:
+    // Todo: second
 //    val barFoo = Bar(2, "second").asJson
 //    val bar = barFoo.as[Bar]
-//    println("bar = " + bar)
+//    println("bar = " + bar.toOption)
+
+    // Todo: third
+    val userJson = User(1L, "third", "Curry").asJson(encodeUser)
+    val user = userJson.as[User](decodeUser)
+    println("user = " + user.toOption)
   }
 }
 
---
+++ src/main/scala/simpleCirce/semiAutomaticDerivation/User.scala
@@ -0,0 +1,3 @@
+package simpleCirce.semiAutomaticDerivation
+
+case class User(id: Long, firstName: String, lastName: String)
